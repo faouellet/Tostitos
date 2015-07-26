f4a078f504b7e0f7172b3925977ea4f3efca0e01
faouellet <faouellet@gmail.com>
2015-07-26 17:53:16 -0400

    Finally fixed the Linux build

--- cmake/BoostTestHelpers.cmake
+++ cmake/BoostTestHelpers.cmake
@@ -1,5 +1,5 @@
 # Utility function for automatically adding Boost based unit tests
-# Source: http://ericscottbarr.com/blog/2015/06/driving-boost-dot-test-with-cmake/
+# Inspiration: http://ericscottbarr.com/blog/2015/06/driving-boost-dot-test-with-cmake/
 
 function(add_boost_test SOURCE_FILE_NAME DEPENDENCY_LIB)
     get_filename_component(TEST_EXECUTABLE_NAME ${SOURCE_FILE_NAME} NAME_WE)
@@ -9,6 +9,16 @@ function(add_boost_test SOURCE_FILE_NAME DEPENDENCY_LIB)
                           ${DEPENDENCY_LIB} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
 
     file(READ "${SOURCE_FILE_NAME}" SOURCE_FILE_CONTENTS)
+
+    string(REGEX MATCH "BOOST_FIXTURE_TEST_SUITE\\( *([A-Za-z_0-9]+), [A-Za-z_0-9]+ *\\)"
+           SUITE_MATCHES ${SOURCE_FILE_CONTENTS})
+
+   list(LENGTH SUITE_MATCHES N)
+   if(${N} EQUAL 1)
+       list(GET SUITE_MATCHES 0 SUITE_MATCH)
+       string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+), [A-Za-z_0-9]+ *\\)" "\\1" SUITE_NAME ${SUITE_MATCH})
+   endif()
+
     string(REGEX MATCHALL "BOOST_AUTO_TEST_CASE\\( *([A-Za-z_0-9]+) *\\)" 
            FOUND_TESTS ${SOURCE_FILE_CONTENTS})
 
@@ -17,6 +27,6 @@ function(add_boost_test SOURCE_FILE_NAME DEPENDENCY_LIB)
 
         add_test(NAME "${TEST_EXECUTABLE_NAME}.${TEST_NAME}" 
                  COMMAND ${TEST_EXECUTABLE_NAME}
-                 --run_test=${TEST_NAME} --catch_system_error=yes)
+                 --run_test=${SUITE_NAME}/${TEST_NAME} --catch_system_error=yes)
     endforeach()
 endfunction()
