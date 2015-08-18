95551ba79f50723142f7116bfd41c315f9d27c16
faouellet <faouellet@gmail.com>
2015-08-18 18:38:43 -0400

    TosLang
    - Adding tests for node kinds

--- tests/parser_tests.cpp
+++ tests/parser_tests.cpp
@@ -28,10 +28,12 @@ BOOST_AUTO_TEST_CASE( ParseVarDeclTest )
     const ChildrenNodes& cNodes = pDecl->GetChildrenNodes();
     BOOST_REQUIRE_EQUAL(cNodes.size(), 2);
 
+    BOOST_REQUIRE_EQUAL(cNodes[0]->GetKind(), ASTNode::VAR_DECL);
     const VarDecl* vDecl = dynamic_cast<const VarDecl*>(cNodes[0].get());
     BOOST_REQUIRE(vDecl != nullptr);
     BOOST_REQUIRE_EQUAL(vDecl->GetVarName(), "MyIntVar");
 
+    BOOST_REQUIRE_EQUAL(cNodes[0]->GetKind(), ASTNode::VAR_DECL);
     vDecl = dynamic_cast<const VarDecl*>(cNodes[1].get());
     BOOST_REQUIRE(vDecl != nullptr);
     BOOST_REQUIRE_EQUAL(vDecl->GetVarName(), "MyBoolVar");
@@ -43,29 +45,36 @@ BOOST_AUTO_TEST_CASE( ParseVarInitTest )
     std::unique_ptr<ASTNode> rootNode = parser.ParseProgram("../inputs/varinit.tos");
     BOOST_REQUIRE(rootNode != nullptr);
 
+    BOOST_REQUIRE_EQUAL(rootNode->GetKind(), ASTNode::PROGRAM_DECL);
     const ProgramDecl* pDecl = dynamic_cast<const ProgramDecl*>(rootNode.get());
     BOOST_REQUIRE(pDecl != nullptr);
 
     const ChildrenNodes& cNodes = pDecl->GetChildrenNodes();
     BOOST_REQUIRE_EQUAL(cNodes.size(), 3);
 
+    BOOST_REQUIRE_EQUAL(cNodes[0]->GetKind(), ASTNode::VAR_DECL);
     const VarDecl* vDecl = dynamic_cast<const VarDecl*>(cNodes[0].get());
     BOOST_REQUIRE(vDecl != nullptr);
     BOOST_REQUIRE_EQUAL(vDecl->GetVarName(), "MyIntVar");
+    BOOST_REQUIRE_EQUAL(vDecl->GetInitExpr()->GetKind(), ASTNode::NUMBER_EXPR);
     const NumberExpr* nExpr = dynamic_cast<const NumberExpr*>(vDecl->GetInitExpr().get());
     BOOST_REQUIRE(nExpr != nullptr);
     BOOST_REQUIRE_EQUAL(nExpr->GetValue(), 42);
 
+    BOOST_REQUIRE_EQUAL(cNodes[0]->GetKind(), ASTNode::VAR_DECL);
     vDecl = dynamic_cast<const VarDecl*>(cNodes[1].get());
     BOOST_REQUIRE(vDecl != nullptr);
     BOOST_REQUIRE_EQUAL(vDecl->GetVarName(), "MyTrueVar");
+    BOOST_REQUIRE_EQUAL(vDecl->GetInitExpr()->GetKind(), ASTNode::BOOLEAN_EXPR);
     const BooleanExpr* bExpr = dynamic_cast<const BooleanExpr*>(vDecl->GetInitExpr().get());
     BOOST_REQUIRE(bExpr != nullptr);
     BOOST_REQUIRE_EQUAL(bExpr->GetValue(), true);
 
+    BOOST_REQUIRE_EQUAL(cNodes[0]->GetKind(), ASTNode::VAR_DECL);
     vDecl = dynamic_cast<const VarDecl*>(cNodes[2].get());
     BOOST_REQUIRE(vDecl != nullptr);
     BOOST_REQUIRE_EQUAL(vDecl->GetVarName(), "MyFalseVar");
+    BOOST_REQUIRE_EQUAL(vDecl->GetInitExpr()->GetKind(), ASTNode::BOOLEAN_EXPR);
     bExpr = dynamic_cast<const BooleanExpr*>(vDecl->GetInitExpr().get());
     BOOST_REQUIRE(bExpr != nullptr);
     BOOST_REQUIRE_EQUAL(bExpr->GetValue(), false);
