df48edb34a4ca14e2011b128872ba5023d2d0698
faouellet <faouellet@gmail.com>
2015-11-23 19:14:09 -0500

    TosLang: Updating the AST Visitor with new node handling functions

--- TosLang/Utils/astvisitor.h
+++ TosLang/Utils/astvisitor.h
@@ -64,6 +64,18 @@ namespace TosLang
 
         protected:  // Declarations
             /*
+            * \fn       HandleFunctionDecl
+            * \brief    Handle a node of the FUNCTION_DECL kind
+            */
+            void HandleFunctionDecl() { }
+
+            /*
+            * \fn       HandleParamVarDecl
+            * \brief    Handle a node of the PARAM_VAR_DECL kind
+            */
+            void HandleParamVarDecl() { }
+
+            /*
             * \fn       HandleProgramDecl
             * \brief    Handle a node of the PROGRAM_DECL kind
             */
@@ -77,16 +89,71 @@ namespace TosLang
 
         protected:  // Expressions
             /*
+            * \fn       HandleBinrayExpr
+            * \brief    Handle a node of the BINARY_EXPR kind
+            */
+            void HandleBinaryExpr() { }
+
+            /*
             * \fn       HandleBooleanExpr
             * \brief    Handle a node of the BOOLEAN_EXPR kind
             */
             void HandleBooleanExpr() { }
 
             /*
+            * \fn       HandleCallExpr
+            * \brief    Handle a node of the CALL_EXPR kind
+            */
+            void HandleCallExpr() { }
+
+            /*
+            * \fn       HandleIdentifierExpr
+            * \brief    Handle a node of the IDENTIFIER_EXPR kind
+            */
+            void HandleIdentifierExpr() { }
+
+            /*
             * \fn       HandleNumberExpr
             * \brief    Handle a node of the NUMBER_EXPR kind
             */
             void HandleNumberExpr() { }
+        
+        protected:  // Statements
+            /*
+            * \fn       HandleCompoundStmt
+            * \brief    Handle a node of the COMPOUND_STMT kind
+            */
+            void HandleCompoundStmt() { }
+
+            /*
+            * \fn       HandleIfStmt
+            * \brief    Handle a node of the IF_STMT kind
+            */
+            void HandleIfStmt() { }
+
+            /*
+            * \fn       HandlePrintStmt
+            * \brief    Handle a node of the PRINT_STMT kind
+            */
+            void HandlePrintStmt() { }
+
+            /*
+            * \fn       HandleReturnStmt
+            * \brief    Handle a node of the RETURN_STMT kind
+            */
+            void HandleReturnStmt() { }
+
+            /*
+            * \fn       HandleScanStmt
+            * \brief    Handle a node of the SCAN_STMT kind
+            */
+            void HandleScanStmt() { }
+
+            /*
+            * \fn       HandleWhileStmt
+            * \brief    Handle a node of the WHILE_STMT kind
+            */
+            void HandleWhileStmt() { }
 
         private:
             /*
@@ -108,17 +175,53 @@ namespace TosLang
             {
                 switch (node->GetKind())
                 {
+                    ////// Declarations //////
+                case FrontEnd::ASTNode::NodeKind::FUNCTION_DECL:
+                    GetDerived().HandleFunctionDecl();
+                    break;
+                case FrontEnd::ASTNode::NodeKind::PARAM_VAR_DECL:
+                    GetDerived().HandleParamVarDecl();
+                    break;
                 case FrontEnd::ASTNode::NodeKind::PROGRAM_DECL:
                     GetDerived().HandleProgramDecl();
                     break;
                 case FrontEnd::ASTNode::NodeKind::VAR_DECL:
                     GetDerived().HandleVarDecl();
                     break;
+                    ////// Expressions //////
+                case FrontEnd::ASTNode::NodeKind::BINARY_EXPR:
+                    GetDerived().HandleBinaryExpr();
+                    break;
                 case FrontEnd::ASTNode::NodeKind::BOOLEAN_EXPR:
                     GetDerived().HandleBooleanExpr();
                     break;
+                case FrontEnd::ASTNode::NodeKind::CALL_EXPR:
+                    GetDerived().HandleCallExpr();
+                    break;
+                case FrontEnd::ASTNode::NodeKind::IDENTIFIER_EXPR:
+                    GetDerived().HandleIdentifierExpr();
+                    break;
                 case FrontEnd::ASTNode::NodeKind::NUMBER_EXPR:
                     GetDerived().HandleNumberExpr();
+                    break;
+                    ////// Statements //////
+                case FrontEnd::ASTNode::NodeKind::COMPOUND_STMT:
+                    GetDerived().HandleCompoundStmt();
+                    break;
+                case FrontEnd::ASTNode::NodeKind::IF_STMT:
+                    GetDerived().HandleIfStmt();
+                    break;
+                case FrontEnd::ASTNode::NodeKind::PRINT_STMT:
+                    GetDerived().HandlePrintStmt();
+                    break;
+                case FrontEnd::ASTNode::NodeKind::RETURN_STMT:
+                    GetDerived().HandleReturnStmt();
+                    break;
+                case FrontEnd::ASTNode::NodeKind::SCAN_STMT:
+                    GetDerived().HandleScanStmt();
+                    break;
+                case FrontEnd::ASTNode::NodeKind::WHILE_STMT:
+                    GetDerived().HandleWhileStmt();
                     break;
                 default:
                     break;
