9c907f7e520440d8b4113ad604d017f5929af867
faouellet <faouellet@gmail.com>
2015-12-18 21:26:53 -0500

    TosLang: Started putting together the compiler driver with what has been done so far

--- TosLang/driver.cpp
+++ TosLang/driver.cpp
@@ -1,6 +1,30 @@
+#include "AST/ast.h"
+#include "Parse/parser.h"
+#include "Sema/symbolcollector.h"
+#include "Sema/symboltable.h"
+#include "Sema/typechecker.h"
+
+// TODO: include codegen
+
 #include <iostream>
+
+static void Compile(const std::string& programFile)
+{
+    TosLang::FrontEnd::Parser parser;
+    auto programAST = parser.ParseProgram(programFile);
+    if (programAST == nullptr)
+        return;
+ 
+    auto symbolTable = std::make_shared<TosLang::FrontEnd::SymbolTable>();
+    TosLang::FrontEnd::SymbolCollector sCollector{ symbolTable };
+    sCollector.Run(programAST);
+
+    TosLang::FrontEnd::TypeChecker tChecker{ symbolTable };
+    tChecker.Run(programAST);
+    if (tChecker.GetErrorCount() != 0)
+        return;
+}
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
 }
