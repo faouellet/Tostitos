01c4ff66b15b779aef0d3dcf3f86d9bdd3f83be0
faouellet <faouellet@gmail.com>
2016-02-22 19:01:28 -0500

    TosLang: Finished the ASTPrinter

--- TosLang/Utils/astprinter.h
+++ TosLang/Utils/astprinter.h
@@ -41,7 +41,7 @@ namespace TosLang
 
         protected:  // Declarations
             /*
-            * \fn       HandleNumberExpr
+            * \fn       HandleProgramDecl
             * \brief    Prints a node of the PROGRAM_DECL kind
             */
             void HandleProgramDecl()
@@ -51,7 +51,22 @@ namespace TosLang
             }
 
             /*
-            * \fn       HandleNumberExpr
+            * \fn       HandleFunctionDecl
+            * \brief    Prints a node of the FUNCTION_DECL kind
+            */
+            void HandleFunctionDecl()
+            {
+                const FrontEnd::FunctionDecl* fDecl = dynamic_cast<const FrontEnd::FunctionDecl*>(this->mCurrentNode);
+                assert(fDecl != nullptr);
+
+                Indent();
+                mStream << "FunctionDecl: " << fDecl->GetName()
+                        << " Return Type: " << static_cast<std::underlying_type<Common::Type>::type>(fDecl->GetReturnType())
+                        << "\n";
+            }
+
+            /*
+            * \fn       HandleVarDecl
             * \brief    Prints a node of the VAR_DECL kind
             */
             void HandleVarDecl()
@@ -66,6 +81,19 @@ namespace TosLang
             }
 
         protected:  // Expressions
+            /*
+            * \fn       HandleBinaryOpExpr
+            * \brief    Prints a node of the BINARY_EXPR kind
+            */
+            void HandleBinaryOpExpr()
+            {
+                const FrontEnd::BinaryOpExpr* bExpr = dynamic_cast<const FrontEnd::BinaryOpExpr*>(this->mCurrentNode);
+                assert(bExpr != nullptr);
+
+                Indent();
+                mStream << "BinaryOpExpr: " << bExpr->GetOperation() << "\n";
+            }
+
             /*
             * \fn       HandleNumberExpr
             * \brief    Prints a node of the BOOLEAN_EXPR kind
@@ -87,6 +115,19 @@ namespace TosLang
             }
 
             /*
+            * \fn       HandleCallExpr
+            * \brief    Prints a node of the CALL_EXPR kind
+            */
+            void HandleCallExpr()
+            {
+                const FrontEnd::CallExpr* cExpr = dynamic_cast<const FrontEnd::CallExpr*>(this->mCurrentNode);
+                assert(cExpr != nullptr);
+
+                Indent();
+                mStream << "CallExpr: " << cExpr->GetCalleeName() << "\n";
+            }
+
+            /*
             * \fn       HandleNumberExpr
             * \brief    Prints a node of the NUMBER_EXPR kind
             */
@@ -97,6 +138,80 @@ namespace TosLang
 
                 Indent();
                 mStream << "NumberExpr: " << nExpr->GetValue() << "\n";
+            }
+
+            /*
+            * \fn       HandleStringExpr
+            * \brief    Prints a node of the STRING_EXPR kind
+            */
+            void HandleStringExpr()
+            {
+                const FrontEnd::StringExpr* sExpr = dynamic_cast<const FrontEnd::StringExpr*>(this->mCurrentNode);
+                assert(sExpr != nullptr);
+
+                Indent();
+                mStream << "StringExpr: " << sExpr->GetName() << "\n";
+            }
+
+        protected:  // Statements
+            /*
+            * \fn       HandleCompoundStmt
+            * \brief    Prints a node of the COMPOUND_STMT kind
+            */
+            void HandleCompoundStmt()
+            {
+                Indent();
+                mStream << "CompoundStmt\n";
+            }
+
+            /*
+            * \fn       HandleIfStmt
+            * \brief    Prints a node of the IF_STMT kind
+            */
+            void HandleIfStmt()
+            {
+                Indent();
+                mStream << "IfStmt\n";
+            }
+
+            /*
+            * \fn       HandlePrintStmt
+            * \brief    Prints a node of the PRINT_STMT kind
+            */
+            void HandlePrintStmt()
+            {
+                Indent();
+                mStream << "PrintStmt\n";
+            }
+
+            /*
+            * \fn       HandleReturnStmt
+            * \brief    Prints a node of the RETURN_STMT kind
+            */
+            void HandleReturnStmt()
+            {
+                Indent();
+                mStream << "ReturnStmt\n";
+            }
+
+            /*
+            * \fn       HandleScanStmt
+            * \brief    Prints a node of the SCAN_STMT kind
+            */
+            void HandleScanStmt()
+            {
+                Indent();
+                mStream << "ScanStmt\n";
+            }
+
+            /*
+            * \fn       HandleWhileStmt
+            * \brief    Prints a node of the WHILE_STMT kind
+            */
+            void HandleWhileStmt()
+            {
+                Indent();
+                mStream << "WhileStmt\n";
             }
 
         private:
