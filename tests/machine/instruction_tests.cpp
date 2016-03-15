368c51ad927d4d625393322042d0d6539cf46ce4
faouellet <faouellet@gmail.com>
2016-03-15 18:44:07 -0400

    TosLang: Completely removing the test that was causing a failure on the build machines

--- tests/machine/instruction_tests.cpp
+++ tests/machine/instruction_tests.cpp
@@ -14,30 +14,6 @@
 
 using namespace MachineEngine::ProcessorSpace;
 
-// TODO: Should this be modified to test VirtualInstruction?
-/*BOOST_AUTO_TEST_CASE( InstructionTypeTest )
-{
-    std::vector<Instruction::InstructionOpCode> types = {
-        Instruction::NO_OP, Instruction::JUMP, Instruction::CALL, Instruction::RET, 
-        Instruction::LOAD_IMM, Instruction::MOV, Instruction::STORE, Instruction::PUSH,
-        Instruction::POP, Instruction::PUSHALL, Instruction::POPALL, Instruction::ADD_IMM, 
-        Instruction::ADD, Instruction::SUB_IMM, Instruction::SUB, Instruction::AND_IMM, 
-        Instruction::AND, Instruction::TST, Instruction::OR_IMM, Instruction::OR, 
-        Instruction::XOR_IMM, Instruction::XOR, Instruction::MUL_IMM, Instruction::MUL, 
-        Instruction::DIV_IMM, Instruction::DIV, Instruction::SHIFT, Instruction::MOD_IMM,
-        Instruction::MOD, Instruction::NOT_IMM, Instruction::NOT, Instruction::NEG_IMM, 
-        Instruction::NEG
-    };
-
-    for (auto& type : types)
-    {
-        Instruction inst(type);
-        BOOST_REQUIRE_EQUAL(inst.GetType(), type);
-        BOOST_REQUIRE_EQUAL(inst.GetOpcode(), type);
-        BOOST_REQUIRE(!inst.IsInplace());
-    }
-}*/
-
 BOOST_AUTO_TEST_CASE( InstructionGetSetTest )
 {
     const unsigned addInst = 0x52000000;
