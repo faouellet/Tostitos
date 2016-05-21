4e82b4e37096760953cad405db05b9c8bda3e1cd
faouellet <faouellet@gmail.com>
2016-05-20 21:20:49 -0400

    TosLang: Adding intrepreter call stack stub

---
+++ TosLang/Interpreter/callstack.h
@@ -0,0 +1,16 @@
+#ifndef CALL_STACK_H__TOSTITOS
+#define CALL_STACK_H__TOSTITOS
+
+#include <stack>
+
+class StackFrame
+{
+public:
+
+private:
+
+};
+
+using CallStack = std::stack<StackFrame>;
+
+#endif // CALL_STACK_H__TOSTITOS
