4fd774dbe6335d1e2c92e943de003de528461436
faouellet <faouellet@gmail.com>
2016-06-08 20:06:10 -0400

    TroLang: Started working on system level tests for the interpreter
    - Added a test runner
    - Updated CMakeLists.txt
    - Added tests

---
+++ tests/testrunner.py
@@ -0,0 +1,22 @@
+import os
+import re
+import subprocess
+
+def run_tests():
+	expected_regex = re.compile("EXPECTED: (?P<result>.*)")
+
+	for program in os.listdir("./interpreter"):
+		# Fetch the expected result(s)
+		f = open(program)
+		expected_results = [m.group('result') for l in f.readlines() for m in [expected_regex.search(l)] if m]
+
+		# Execute the program
+		p = subprocess.Popen(['TosLang', '-interpret', program], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		output, err = p.communicate()
+
+		# Report
+		test_passed = output == expected_results
+		print "TEST: {}: {}".format(program, "PASSED" if test_passed else "FAILED")
+
+if __name__ == 'main'
+	run_tests()
