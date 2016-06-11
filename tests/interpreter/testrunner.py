7f0426003003f45385c2840a98f3b52f6c09c6d5
faouellet <faouellet@gmail.com>
2016-06-10 21:57:13 -0400

    Travis-CI: Adding system-level tests to the continuous integration process

--- tests/interpreter/testrunner.py
+++ tests/interpreter/testrunner.py
@@ -3,22 +3,28 @@
 import os
 import re
 import subprocess
+import sys
 
-def run_tests():
+import pdb
+
+def run_tests(test_dir, exec_dir):
 	expected_regex = re.compile("EXPECTED: (?P<result>.*)")
-
-	for program in os.listdir("./interpreter/programs"):
+	
+	for program in os.listdir(test_folder):
 		# Fetch the expected result(s)
-		f = open(program)
+		f = open(os.path.join(test_dir, program))
 		expected_results = [m.group('result') for l in f.readlines() for m in [expected_regex.search(l)] if m]
 
 		# Execute the program
-		p = subprocess.Popen(['TosLang', '-interpret', program], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		p = subprocess.Popen([os.path.join(exec_dir, 'TosLang'), '-interpret', program], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		output, err = p.communicate()
 
 		# Report
 		test_passed = output == expected_results
 		print "TEST: {}: {}".format(program, "PASSED" if test_passed else "FAILED")
 
-if __name__ == 'main'
-	run_tests()
+
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print "ERROR"
+	run_tests(sys.argv[1], sys.argv[2])
